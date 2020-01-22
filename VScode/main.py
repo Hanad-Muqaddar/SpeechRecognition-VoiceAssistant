5a3b19a621e2a90454371148cebbdfed9b75f57d
syedhanad <syedhanad786@gmail.com>
2020-01-23 01:54:23 +0500

    First Commit

---
+++ VScode/main.py
@@ -0,0 +1,14 @@
+print("hanad")
+print("hello how r u?")
+
+#hanad wasa good boy
+#ali was a good boy
+#bial was a good boy
+list = ["ali", "hanad", "bilal", "rehan", "kamil", "shah", "aqib", "asim" ]
+print(list[0:4])
+for name in list:
+    print(name)
+ 
+
+
+
